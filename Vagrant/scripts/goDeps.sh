8598320931c017cec455aac203b0cbbd05c4dd1c
Ryan Endacott <rzeg24@gmail.com>
2015-05-18 00:33:19 -0500

    vagrant: Install Go 1.4.2 instead of default ubuntu Go.

--- Vagrant/scripts/goDeps.sh
+++ Vagrant/scripts/goDeps.sh
@@ -1,4 +1,5 @@
 export GOPATH=/home/vagrant/go;
+export PATH=$PATH:/usr/local/go/bin;
 go get -d github.com/dancannon/gorethink;
 go get -d github.com/codegangsta/negroni;
 go get -d github.com/gorilla/mux;
