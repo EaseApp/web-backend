dec7a8eafa942f60b9cd02ab769eca0a43b4f416
felttrip <nthompson55@gmail.com>
2015-10-02 19:43:03 -0500

    travis didn't like my syntax

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 chmod +x bin/main
-bash <(curl --ftp-create-dirs -T bin/main -u $FTP_USER:$FTP_PASSWORD ftp://ease-62q56ueo.cloudapp.net)
+curl --ftp-create-dirs -T bin/main -u $FTP_USER:$FTP_PASSWORD ftp://ease-62q56ueo.cloudapp.net
