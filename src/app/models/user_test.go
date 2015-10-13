f62b36e25a3e7120c975527bf67a3c5100109720
Ryan Endacott <rzeg24@gmail.com>
2015-10-13 00:58:31 -0500

    Add extra logging statement after insert.

--- src/app/models/user_test.go
+++ src/app/models/user_test.go
@@ -84,5 +84,6 @@ func getDBClient(t *testing.T) *db.Client {
 	r.DB("test").Table("users").Insert(map[string]string{"hello": "world"}).RunWrite(client.Session)
 	r.DB("test").Table("users").Insert(struct{ prop string }{prop: "I am a string."}).RunWrite(client.Session)
 	r.DB("test").Table("users").Insert(User{Username: "hi", CreatedAt: time.Now()}).RunWrite(client.Session)
+	t.Log("I DID THE USER STUFF AND DID NOT FAIL.")
 	return client
 }
