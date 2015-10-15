69904ec977b48cf9d6d3c2801f00ca47aaad1ff0
Ryan Endacott <rzeg24@gmail.com>
2015-10-15 15:57:00 -0500

    Skeleton for RequireAPIToken.

--- src/app/controllers/helpers/helpers.go
+++ src/app/controllers/helpers/helpers.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+
+	"github.com/EaseApp/web-backend/src/app/models"
 )
 
 type errorResponse struct {
@@ -17,4 +19,19 @@ func SendError(errorCode int, err error, w http.ResponseWriter) {
 	log.Printf("Error: Returning status code %d with error message %s.\n", errorCode, err)
 	resp := errorResponse{ErrCode: errorCode, ErrMessage: err.Error()}
 	json.NewEncoder(w).Encode(resp)
+}
+
+// RequireAPIToken requires that the given route has a valid APIToken
+// and passes the user it represents to the handler.
+func RequireAPIToken(
+	handler func(http.ResponseWriter, *http.Request, *models.User)) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		// TODO: implement this.
+		// Make a new UserQuerier function to find a user by API token.
+		// If one isn't found, do:
+		// SendError(http.StatusUnAuthorized, errors.New("Invalid API token.", w)
+		// If one is found, call handler like the below code:
+		var user *models.User
+		handler(w, req, user)
+	}
 }
