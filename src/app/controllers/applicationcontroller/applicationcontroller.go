5c01c5548201177cf982f9396c820912818ed819
Ryan Endacott <rzeg24@gmail.com>
2015-11-04 21:03:33 -0600

    Naive impls of CRUD handlers.

--- src/app/controllers/applicationcontroller/applicationcontroller.go
+++ src/app/controllers/applicationcontroller/applicationcontroller.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/EaseApp/web-backend/src/app/controllers/helpers"
 	"github.com/EaseApp/web-backend/src/app/models"
+	"github.com/EaseApp/web-backend/src/lib"
 	"github.com/gorilla/mux"
 )
 
@@ -55,4 +56,90 @@ func DeleteApplicationHandler(w http.ResponseWriter, req *http.Request, user *mo
 	}
 
 	json.NewEncoder(w).Encode(user.Applications)
+}
+
+// appDataReqParams holds the params needed for the data handlers below.
+type appDataReqParams struct {
+	PathStr string      `json:"path"`
+	Data    interface{} `json:"data"`
+	Path    lib.Path    `json:"-"`
+}
+
+var successResponse = struct {
+	Success bool `json:"success"`
+}{true}
+
+// SaveApplicationDataHandler handles saving app data.
+func SaveApplicationDataHandler(w http.ResponseWriter, req *http.Request, app *models.Application) {
+	params, err := parseAppDataParams(w, req)
+	if err != nil {
+		return
+	}
+
+	err = querier.SaveApplicationData(app, params.Path, params.Data)
+	if err != nil {
+		friendlyErr := errors.New("Failed to save application data")
+		log.Println(friendlyErr, ": ", err)
+		helpers.SendError(http.StatusInternalServerError, friendlyErr, w)
+		return
+	}
+
+	json.NewEncoder(w).Encode(successResponse)
+}
+
+// ReadApplicationDataHandler handles reading app data.
+func ReadApplicationDataHandler(w http.ResponseWriter, req *http.Request, app *models.Application) {
+	params, err := parseAppDataParams(w, req)
+	if err != nil {
+		return
+	}
+
+	data, err := querier.ReadApplicationData(app, params.Path)
+	if err != nil {
+		friendlyErr := errors.New("Failed to read application data")
+		log.Println(friendlyErr, ": ", err)
+		helpers.SendError(http.StatusInternalServerError, friendlyErr, w)
+		return
+	}
+
+	json.NewEncoder(w).Encode(data)
+}
+
+// DeleteApplicationDataHandler handles deleting app data.
+func DeleteApplicationDataHandler(w http.ResponseWriter, req *http.Request, app *models.Application) {
+	params, err := parseAppDataParams(w, req)
+	if err != nil {
+		return
+	}
+
+	err = querier.DeleteApplicationData(app, params.Path)
+	if err != nil {
+		friendlyErr := errors.New("Failed to delete application data")
+		log.Println(friendlyErr, ": ", err)
+		helpers.SendError(http.StatusInternalServerError, friendlyErr, w)
+		return
+	}
+
+	json.NewEncoder(w).Encode(successResponse)
+}
+
+// parseAppDataParams parses the given app data params and sends an error if they're invalid.
+func parseAppDataParams(w http.ResponseWriter, req *http.Request) (appDataReqParams, error) {
+	var params appDataReqParams
+
+	err := json.NewDecoder(req.Body).Decode(&params)
+	if err != nil {
+		friendlyErr := errors.New("Invalid JSON: " + err.Error())
+		helpers.SendError(http.StatusBadRequest, friendlyErr, w)
+		return params, err
+	}
+
+	params.Path, err = lib.ParsePath(params.PathStr)
+	if err != nil {
+		friendlyErr := errors.New("Invalid Path: " + err.Error())
+		helpers.SendError(http.StatusBadRequest, friendlyErr, w)
+		return params, err
+	}
+
+	return params, nil
 }
